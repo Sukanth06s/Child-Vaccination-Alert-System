c946caa1e6b787718268ebae3f48953dd3289db1
Nachikaet <nachikaet@gmail.com>
2026-01-19 13:10:43 +0530

    pushing bcg alone

---
+++ bcg.py
@@ -0,0 +1,24 @@
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,recall_score
+import pandas as pd
+input_path=r"C:\webp proj\children\vaccine_datasets\BCG.csv"
+output_path=r"C:\Users\Admin\Desktop\Child-Vaccination-Alert-System\bcg.json"
+df=pd.read_csv(input_path)
+x=df[['v012','v106','v025','v190','v101','b19','b4','bord']]
+y=df['label']
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+model = xgb.XGBClassifier(
+    n_estimators=200,
+    max_depth=5,
+    learning_rate=0.05,
+    random_state=42,
+    eval_metric="logloss"
+)
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
+accuracy=accuracy_score(y_test,y_pred)
+recall=recall_score(y_test,y_pred)
+print("Accuracy is:",accuracy)
+print("Recall is:",recall)
+model.save_model(output_path)
